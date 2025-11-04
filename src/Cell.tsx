8e2fdfaf360b4755577f11954ccca3d7f95bf5a7
didier <didier.boff@axess.fr>
2025-11-04 21:54:04 +0100

    Fix flickering on the first click

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, memo } from 'react';
 import { Text } from '@react-three/drei';
-import { useThree } from '@react-three/fiber';
 
 interface CellProps {
   position: [number, number, number];
@@ -8,12 +7,12 @@ interface CellProps {
   onClick: () => void;
 }
 
-const Cell: React.FC<CellProps> = ({ position, value, onClick }) => {
+const Cell: React.FC<CellProps> = memo(({ position, value, onClick }) => {
   const [hovered, setHovered] = useState(false);
-  
+
   return (
-    <mesh 
-      position={position} 
+    <mesh
+      position={position}
       onClick={(e) => {
         e.stopPropagation();
         onClick();
@@ -28,85 +27,86 @@ const Cell: React.FC<CellProps> = ({ position, value, onClick }) => {
       }}
       castShadow
       receiveShadow
+      frustumCulled={false}
     >
       <boxGeometry args={[0.9, 0.9, 0.9]} />
-      <meshStandardMaterial 
-        color={value === 'X' ? '#ff4757' : value === 'O' ? '#2ed573' : hovered ? '#ffa502' : '#add8e6'} 
+      <meshStandardMaterial
+        color={value === 'X' ? '#ff4757' : value === 'O' ? '#2ed573' : hovered ? '#ffa502' : '#add8e6'}
         wireframe={false}
-        transparent
-        opacity={value === '' ? 0.5 : 0.8}
+        transparent={value === ''}
+        opacity={value === '' ? 0.3 : 1.0}
       />
-      {value && (
-        <>
-          {/* Front face */}
-          <Text
-            position={[0, 0, 0.46]} // Slightly in front of the cube surface
-            fontSize={0.5}
-            color="white"
-            anchorX="center"
-            anchorY="middle"
-          >
-            {value}
-          </Text>
-          {/* Back face */}
-          <Text
-            position={[0, 0, -0.46]} // Slightly behind the cube surface
-            fontSize={0.5}
-            color="white"
-            anchorX="center"
-            anchorY="middle"
-            rotation={[0, Math.PI, 0]} // Rotate 180 degrees around Y axis
-          >
-            {value}
-          </Text>
-          {/* Right face */}
-          <Text
-            position={[0.46, 0, 0]} // Slightly to the right of the cube surface
-            fontSize={0.5}
-            color="white"
-            anchorX="center"
-            anchorY="middle"
-            rotation={[0, -Math.PI / 2, 0]} // Rotate -90 degrees around Y axis
-          >
-            {value}
-          </Text>
-          {/* Left face */}
-          <Text
-            position={[-0.46, 0, 0]} // Slightly to the left of the cube surface
-            fontSize={0.5}
-            color="white"
-            anchorX="center"
-            anchorY="middle"
-            rotation={[0, Math.PI / 2, 0]} // Rotate 90 degrees around Y axis
-          >
-            {value}
-          </Text>
-          {/* Top face */}
-          <Text
-            position={[0, 0.46, 0]} // Slightly above the cube surface
-            fontSize={0.5}
-            color="white"
-            anchorX="center"
-            anchorY="middle"
-            rotation={[-Math.PI / 2, 0, 0]} // Rotate -90 degrees around X axis
-          >
-            {value}
-          </Text>
-          {/* Bottom face */}
-          <Text
-            position={[0, -0.46, 0]} // Slightly below the cube surface
-            fontSize={0.5}
-            color="white"
-            anchorX="center"
-            anchorY="middle"
-            rotation={[Math.PI / 2, 0, 0]} // Rotate 90 degrees around X axis
-          >
-            {value}
-          </Text>
-        </>
-      )}
+      <>
+        {/* Front face */}
+        <Text
+          position={[0, 0, 0.46]} // Slightly in front of the cube surface
+          fontSize={0.5}
+          color="white"
+          anchorX="center"
+          anchorY="middle"
+        >
+          {value}
+        </Text>
+        {/* Back face */}
+        <Text
+          position={[0, 0, -0.46]} // Slightly behind the cube surface
+          fontSize={0.5}
+          color="white"
+          anchorX="center"
+          anchorY="middle"
+          rotation={[0, Math.PI, 0]} // Rotate 180 degrees around Y axis
+        >
+          {value}
+        </Text>
+        {/* Right face */}
+        <Text
+          position={[0.46, 0, 0]} // Slightly to the right of the cube surface
+          fontSize={0.5}
+          color="white"
+          anchorX="center"
+          anchorY="middle"
+          rotation={[0, -Math.PI / 2, 0]} // Rotate -90 degrees around Y axis
+        >
+          {value}
+        </Text>
+        {/* Left face */}
+        <Text
+          position={[-0.46, 0, 0]} // Slightly to the left of the cube surface
+          fontSize={0.5}
+          color="white"
+          anchorX="center"
+          anchorY="middle"
+          rotation={[0, Math.PI / 2, 0]} // Rotate 90 degrees around Y axis
+        >
+          {value}
+        </Text>
+        {/* Top face */}
+        <Text
+          position={[0, 0.46, 0]} // Slightly above the cube surface
+          fontSize={0.5}
+          color="white"
+          anchorX="center"
+          anchorY="middle"
+          rotation={[-Math.PI / 2, 0, 0]} // Rotate -90 degrees around X axis
+        >
+          {value}
+        </Text>
+        {/* Bottom face */}
+        <Text
+          position={[0, -0.46, 0]} // Slightly below the cube surface
+          fontSize={0.5}
+          color="white"
+          anchorX="center"
+          anchorY="middle"
+          rotation={[Math.PI / 2, 0, 0]} // Rotate 90 degrees around X axis
+        >
+          {value}
+        </Text>
+      </>
     </mesh>
   );
-};
+});
+
+Cell.displayName = 'Cell';
 
 export default Cell;
